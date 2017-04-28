eb1d2157e9fed1aafcb5582fb0b1315cff90d278
keanedawg <cfife@chicagoventure.com>
2017-04-27 23:39:53 -0500

    added some backbone code. Binary Exponentiation due next.

--- elisp-log.el
+++ elisp-log.el
@@ -1,4 +1,14 @@
+(defun prec_lg (val base iters)
+  ; https://en.wikipedia.org/wiki/Logarithm#Calculation
+  (let ((z (/ (- (float val) 1.0) (+ (float val) 1.0) )) (i 0))
+    (while (< i iters)
+      ()
 
+      (setq i (1+ i)))
+    i
+    ))
+
+(prec_lg 8 2 3)
 
 
 
