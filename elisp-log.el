0dcb9c21b6c9c95ff92f20b7ec7d162c28fd861b
keanedawg <cfife@chicagoventure.com>
2017-04-29 15:18:05 -0500

    very buggy, but the code is churned out

--- elisp-log.el
+++ elisp-log.el
@@ -1,12 +1,14 @@
 (defun prec_lg (val base iters)
   ; https://en.wikipedia.org/wiki/Logarithm#Calculation
-  (let ((z (/ (- (float val) 1.0) (+ (float val) 1.0) )) (i 0))
+  ; O(n^3)
+  ; z, reuslt of (z - 1) / (z + 1), i = iteration count, sum = final output, coeff = 1, 1/3, 1/5, 1/7...
+  (let ((z (/ (- (float val) 1.0) (+ (float val) 1.0) )) (i 0) (sum 1) (coeff 1))
     (while (< i iters)
-      ()
-
-      (setq i (1+ i)))
-    i
-    ))
+      (setq coeff (/ 1 (+ 1 (* 2 i)))
+            sum 2
+            sum (* z sum)
+            i (+ i 1)))
+    i))
 
 (prec_lg 8 2 3)
 
