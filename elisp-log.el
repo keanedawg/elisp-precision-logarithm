adeac221cdc187605d1c4ed2c04cd4bdebd5f8cf
keanedawg <cfife@chicagoventure.com>
2017-04-29 16:10:48 -0500

    Works for any base. But does not use calc library to assist in precision yet

--- elisp-log.el
+++ elisp-log.el
@@ -1,4 +1,4 @@
-(defun prec_lg (val base iters)
+(defun aht_ln (val iters)
   ; https://en.wikipedia.org/wiki/Logarithm#Calculation
   ; O(n^3)
   ; z, reuslt of (z - 1) / (z + 1), i = iteration count, sum = final output, coeff = 1, 1/3, 1/5, 1/7...
@@ -8,10 +8,12 @@
             sum (+ coeff (* z sum))
             sum (* z sum)
             i (- i 2)))
-    (setq sum (* 2.0 sum))
-    sum))
+    (setq sum (* 2.0 sum))))
 
-(prec_lg 8 2 20)
+(defun prec_lg (val base iters)
+  (/ (aht_ln base iters) (aht_ln val iters)))
+
+(prec_lg 2 8 200)
 
 
 
